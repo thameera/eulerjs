eee4b340062f10ddaf19ac14aa37787290d8e5ec
Thameera Senanayaka <thameera123@gmail.com>
2014-01-15 01:08:57 +0530

    Problem 1
    Signed-off-by: Thameera Senanayaka <thameera123@gmail.com>

--- problems/1.js
+++ problems/1.js
@@ -1,6 +1,10 @@
 module.exports = function(_) {
-  console.log('Problem: NAME_HERE');
+  console.log('Problem: Multiples of 3 and 5');
 
-  console.log('Solution: ');
+  var sol = _.reduce(_.range(1000), function(sum, num) {
+    return sum + (num % 3 && num % 5 ? 0 : num);
+  });
+
+  console.log('Solution: ' + sol );
 };
 
