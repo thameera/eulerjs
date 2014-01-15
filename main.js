9f6f5f8fbebbb71e16c5fe8d8fac82f2eaae952e
Thameera Senanayaka <thameera123@gmail.com>
2014-01-15 22:59:45 +0530

    Add math.js
    Signed-off-by: Thameera Senanayaka <thameera123@gmail.com>

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-var _ = require('lodash');
+var _ = require('lodash'),
+    mathjs = require('mathjs');
+
+var math = mathjs();
 
 var prob = +process.argv.splice(2)[0];
 if (!prob) prob = 1;
+
 console.log('Running problem ' + prob + '...');
-require('./problems/' + prob + '.js')(_);
+require('./problems/' + prob + '.js')(_, math);
 
--- template.js
+++ template.js
@@ -1,4 +1,4 @@
-module.exports = function(_) {
+module.exports = function(_, math) {
   console.log('Problem: NAME_HERE');
 
   console.log('Solution: ');
